4064eb9e6f8a49ca1ae9719459e5fab1bceb7000
kotokawa <kawa@kotokawa.com>
2026-05-31 10:00:08 +0900

    Delete start_watch-2 - Copy.bat

--- start_watch-2 - Copy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" ^
-  -NoProfile -ExecutionPolicy Bypass ^
-  -File "Z:\Screenshot\watch_polling_renbanseiseiroiku-3.ps1"
-pause
